0627737eddce04bcb0a07d37d2b6108d0385ecda
andrunick <andrunickson24@gmail.com>
2021-09-08 06:50:14 -0400

    init

---
+++ MyCustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomCell.swift
+//  Travel-App
+//
+//  Created by Enoch Ko on 3/11/17.
+//  Copyright © 2017 asu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MyCustomCell: UITableViewCell {
+    
+    @IBOutlet weak var myCellPicture: UIImageView!
+    
+    @IBOutlet weak var myCellName: UILabel!
+
+}
